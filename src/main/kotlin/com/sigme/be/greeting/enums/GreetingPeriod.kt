089010ed7460c5e1dbf5b9571e580cdc9c420b12
hyo37009 <hyo37009@gmail.com>
2026-07-31 16:56:54 +0900

    feat: 인사 시간대 열거형 추가
    - 아침·오후·저녁·새벽 시간대 정의 추가
    - 화면 표시와 응답에 활용할 한글 설명 속성 추가

---
+++ src/main/kotlin/com/sigme/be/greeting/enums/GreetingPeriod.kt
@@ -0,0 +1,10 @@
+package com.sigme.be.greeting.enums
+
+enum class GreetingPeriod(
+    val description: String,
+) {
+    MORNING("아침"),
+    AFTERNOON("오후"),
+    EVENING("저녁"),
+    DAWN("새벽"),
+}
